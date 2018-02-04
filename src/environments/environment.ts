66749a83b6da86405338779ac84453138a665129
mrgamon <mrgamon@hotmail.com>
2018-02-04 21:05:24 +0800

    test

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDPu_O02o7LUz7ZYMD6iDV5AhGbJeceOAI",
+        authDomain: "firestarter-e0c8b.firebaseapp.com",
+        databaseURL: "https://firestarter-e0c8b.firebaseio.com",
+        projectId: "firestarter-e0c8b",
+        storageBucket: "firestarter-e0c8b.appspot.com",
+        messagingSenderId: "742163280072"
+    }
+};
